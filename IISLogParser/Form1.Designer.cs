c8c1b6b88dfeddfec3127ed2c87824d077d6aa0c
Truong Nguyen <truong.nguyen@enett.com>
2016-06-23 16:54:14 +1000

    initialize

---
+++ IISLogParser/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace IISLogParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open IIS Logs File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1039, 379);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "2016-06-01 06:32:00";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(319, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "2016-06-01 06:35:00";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(17, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(246, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "/vNettService/vNettService.svc/RS";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1039, 111);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 111);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1039, 379);
+            this.panel2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 490);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ IISLogParser/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IISLogParser
+{
+    public partial class Form1 : Form
+    {
+        public class IISLogEntry
+        {
+            public string DateTime { get; set; }
+            public string Method { get; set; }
+            public string Request { get; set; }
+            public string RequestIP { get; set; }
+
+            public string Query { get; set; }
+
+            public string TimeTaken { get; set; }
+
+            public string Win32Status { get; set; }
+
+            public string SubStatus { get; set; }
+
+            public string Status { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dl = new OpenFileDialog();
+
+            if (dl.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.logs = ParseIISLogFile(dl.FileName);
+                DisplayLog(this.logs, textBox1.Text, textBox2.Text, textBox3.Text);
+            }
+        }
+
+        private void DisplayLog(List<IISLogEntry> list, string from = "", string to = "", string request="")
+        {
+            var query = list.AsEnumerable();
+            if (!string.IsNullOrEmpty(from))
+            {
+                query = query.Where(p => string.Compare(p.DateTime , from) >=0 );
+            }
+
+            if (!string.IsNullOrEmpty(to))
+            {
+                query = query.Where(p => string.CompareOrdinal(p.DateTime, to) <= 0);
+            }
+            if (!string.IsNullOrEmpty(request))
+            {
+                query = query.Where(p => p.Request.ToLower() == request.ToLower());
+            }
+
+            this.dataGridView1.DataSource = query.ToList();
+            this.dataGridView1.AutoGenerateColumns = true;
+            this.dataGridView1.AutoResizeColumns();
+        }
+
+        List<IISLogEntry> logs;
+
+        private List<IISLogEntry> ParseIISLogFile(string filename)
+        {
+            var list = new List<IISLogEntry>();
+            using (FileStream fs = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#")) continue;
+                    try
+                    {
+                        IISLogEntry entry = ParseLog(line);
+                        list.Add(entry);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+            return list;
+        }
+
+        private IISLogEntry ParseLog(string line)
+        {
+            var arr = line.Split(new char[] { ' ' });
+            return new IISLogEntry()
+            {
+                DateTime = arr[0] + ' ' + arr[1],
+                RequestIP = arr[2],
+                Method = arr[3],
+                Request = arr[4],
+                Query= arr[5],
+                TimeTaken = arr[arr.Length-1] ,
+                Win32Status = arr[arr.Length - 2],
+                SubStatus = arr[arr.Length - 3],
+                Status = arr[arr.Length - 4]
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisplayLog(this.logs, textBox1.Text, textBox2.Text, textBox3.Text);
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            foreach (DataGridViewRow Myrow in dataGridView1.Rows)
+            {            //Here 2 cell is target value and 1 cell is Volume
+                if (Myrow.Cells[8].Value.ToString() !="200")// Or your condition 
+                {
+                    Myrow.DefaultCellStyle.BackColor = Color.Red;
+                    Myrow.DefaultCellStyle.ForeColor = Color.White;
+                }
+               
+            }
+        }
+    }
+}
